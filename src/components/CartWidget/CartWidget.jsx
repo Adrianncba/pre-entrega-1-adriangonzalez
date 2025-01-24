8d730143fce5ac734fe8dfa4ae130fcf605c3c40
Adrian Gonzalez <adrianncba2@gmail.com>
2025-01-23 23:38:33 -0300

    Cambio titulo e imagen

--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -3,7 +3,7 @@ import './CartWidget.css';
 function CartWidget() {
     return (
         <div className="cart-widget">
-            <img src="https://previews.123rf.com/images/rastudio/rastudio1601/rastudio160100126/49886914-icono-de-l%C3%ADnea-de-carro-de-compras-para-web-m%C3%B3vil-e-infograf%C3%ADa-vector-el-icono-blanco-en-el.jpg" alt="cart" />
+            <img src="https://www.svgrepo.com/show/415705/cart-checkout-ecommerce-2.svg" alt="cart" />
             <p>0</p>
         </div>
     );
